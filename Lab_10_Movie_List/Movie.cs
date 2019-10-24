17a231cdc8f36a6718545c02b00db06ba25cf314
Johnathon Lubaway <NeoJax@gmail.com>
2019-10-24 15:49:10 -0400

    Initializing the Lab 10 Repo

---
+++ Lab_10_Movie_List/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Lab_10_Movie_List
+{
+    public class Movie
+    {
+        #region fields
+        private string _title;
+        private string _category;
+        #endregion
+
+        #region properties
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public string Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+        #endregion
+
+        #region methods
+        public Movie()
+        {
+        }
+
+        public Movie(string title, string category)
+        {
+            _title = title;
+            _category = category;
+        }
+        #endregion
+    }
+}
---
+++ Lab_10_Movie_List/Program.cs
@@ -0,0 +1,17 @@
+﻿// ===============================
+// AUTHOR     : Jonathan Lubaway
+// CREATE DATE: October 24th, 2019
+// PURPOSE    : List Movies by category
+// ===============================
+using System;
+
+namespace Lab_10_Movie_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
